0b88f7eaa58e4a5d34c8fd1bd2361e1b60ee5097
ROJI <roji.13804@gmail.com>
2024-09-20 15:48:40 +1000

    Initial commit

---
+++ test/app.test.js
@@ -0,0 +1,19 @@
+// test/app.test.js
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app'); // Adjust the path if needed
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+describe('GET /', () => {
+    it('should return Hello, World!', (done) => {
+        chai.request(app)
+            .get('/')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res.text).to.equal('Hello, World!');
+                done();
+            });
+    });
+});
